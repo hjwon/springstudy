0000fd3345402f955c5d8de3c7fe87c26be3c1f9
heejae won <hjwon@soribada.com>
2012-06-24 00:34:34 +0900

    2.5.2 학습 테스트 예제 - JUnit 테스트 오브젝트 테스트 (개선)

--- src/springbook/learningtest/junit/JUnitTest.java
+++ src/springbook/learningtest/junit/JUnitTest.java
@@ -1,27 +1,29 @@
 package springbook.learningtest.junit;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.hasItem;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Test;
 
 public class JUnitTest {
-	static JUnitTest testObject;
+	static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
 	
 	@Test public void test1() {
-		assertThat(this, is(not(sameInstance(testObject))));
-		testObject = this;
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
 	}
 	
 	@Test public void test2() {
-		assertThat(this, is(not(sameInstance(testObject))));
-		testObject = this;
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
 	}
 	
 	@Test public void test3() {
-		assertThat(this, is(not(sameInstance(testObject))));
-		testObject = this;
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
 	}
 }
