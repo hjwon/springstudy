d581cf2d2d201d37411663b0b597dcc847d81cf4
heejae won <hjwon@soribada.com>
2012-06-23 17:39:45 +0900

    2.2.2 테스트의 효율적인 수행과 결과 관리

--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -1,14 +1,27 @@
 package springbook.user.dao;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.sql.SQLException;
 
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import springbook.user.domain.User;
 
 public class UserDaoTest {
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	/*
+	 * JUnit : 자바 테스팅 프레임워크
+	 * 	테스트가 main() 메소드로 만들어졌다는 건 제어권을 직접 갖는다는 의미.
+	 * 	JUnit 프레임워크가 요구하는 두 가지 조건
+	 * 	1. 메소드가 public으로 선언되어야 함
+	 * 	2. @Test라는 애노테이션이 필요
+	 */
+	@Test
+	public void addAndGet() throws ClassNotFoundException, SQLException {
 		ApplicationContext context = new GenericXmlApplicationContext(
 				"applicationContext.xml");
 		UserDao dao = context.getBean("userDao", UserDao.class);
@@ -20,17 +33,13 @@ public class UserDaoTest {
 
 		dao.add(user);
 			
-		System.out.println(user.getId() + " 등록 성공");
-		
 		User user2 = dao.get(user.getId());
-		if (!user.getName().equals(user2.getName())) {
-			System.out.println("테스트 실패 (name)");
-		}
-		else if (!user.getPassword().equals(user2.getPassword())) {
-			System.out.println("테스트 실패 (password)");
-		}
-		else {
-			System.out.println("조회 테스트 성공");
-		}
+		
+		assertThat(user2.getName(), is(user.getName()));
+		assertThat(user2.getPassword(), is(user.getPassword()));
+	}
+	
+	public static void main(String[] args) {
+		JUnitCore.main("springbook.user.dao.UserDaoTest");
 	}
 }
