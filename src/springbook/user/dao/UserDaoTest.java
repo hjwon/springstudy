79a19cf748cdb47372d3bcfa81cd0bea8bbb0f0e
heejae won <hjwon@soribada.com>
2012-06-23 20:10:54 +0900

    2.3.2 테스트 결과의 일관성

--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -25,13 +25,17 @@ public class UserDaoTest {
 		ApplicationContext context = new GenericXmlApplicationContext(
 				"applicationContext.xml");
 		UserDao dao = context.getBean("userDao", UserDao.class);
-
+		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
 		User user = new User();
 		user.setId("hjwon");
 		user.setName("원희재");
 		user.setPassword("winter");
 
 		dao.add(user);
+		assertThat(dao.getCount(), is(1));
 			
 		User user2 = dao.get(user.getId());
 		
