4daa17d228e6b5e9a720c668958e7a336b336e78
heejae won <hjwon@soribada.com>
2012-06-23 21:31:57 +0900

    2.3.3 포괄적인 테스트 스프링의 창시자인 로드 존슨 "항상 네거티브 테스트를 먼저 만들라" 테스트를 작성할 때 부정적인 케이스를 먼저 만드는 습관을 들이는 게 좋다. 존재하는 id가 주어졌을 때 해당 레코드를 정확히 가져오는 가를 테스트하는 것도 중요하지만, 존재하지 않는 id가 주어졌을 때는 어떻게 반응할지를 먼저 결정하고, 이를 확인할 수 있는 테스트를 먼저 만들려고 한다면 예외적인 상황을 빠뜨리지 않는 꼼꼼한 개발이 가능하다

--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import springbook.user.domain.User;
 
@@ -24,23 +25,60 @@ public class UserDaoTest {
 	public void addAndGet() throws ClassNotFoundException, SQLException {
 		ApplicationContext context = new GenericXmlApplicationContext(
 				"applicationContext.xml");
+		
 		UserDao dao = context.getBean("userDao", UserDao.class);
+		User user1 = new User("gyumee", "박성철", "springno1");
+		User user2 = new User("leegw700", "이길원", "springno2");
 		
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
 		
-		User user = new User();
-		user.setId("hjwon");
-		user.setName("원희재");
-		user.setPassword("winter");
-
-		dao.add(user);
-		assertThat(dao.getCount(), is(1));
+		dao.add(user1);
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
 			
-		User user2 = dao.get(user.getId());
+		User userget1 = dao.get(user1.getId());
+		assertThat(userget1.getName(), is(user1.getName()));
+		assertThat(userget1.getPassword(), is(user1.getPassword()));
 		
-		assertThat(user2.getName(), is(user.getName()));
-		assertThat(user2.getPassword(), is(user.getPassword()));
+		User userget2 = dao.get(user2.getId());
+		assertThat(userget2.getName(), is(user2.getName()));
+		assertThat(userget2.getPassword(), is(user2.getPassword()));
+	}
+	
+	@Test
+	public void count() throws ClassNotFoundException, SQLException {
+		ApplicationContext context = new GenericXmlApplicationContext(
+				"applicationContext.xml");
+		
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		User user1 = new User("gyumee", "박성철", "springno1");
+		User user2 = new User("leegw700", "이길원", "springno2");
+		User user3 = new User("bumjin", "박범진", "springno3");
+		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		dao.add(user1);
+		assertThat(dao.getCount(), is(1));
+		
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
+		
+		dao.add(user3);
+		assertThat(dao.getCount(), is(3));
+	}
+	
+	@Test(expected=EmptyResultDataAccessException.class)
+	public void getUserFailure() throws ClassNotFoundException, SQLException {
+		ApplicationContext context = new GenericXmlApplicationContext(
+				"applicationContext.xml");
+		
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		dao.get("unknown_id");
 	}
 	
 	public static void main(String[] args) {
