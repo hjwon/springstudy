eed09b113fa9f62fd018b1063e25b17e028971ab
heejae won <hjwon@soribada.com>
2012-06-24 22:31:51 +0900

    3.2.2 분리와 재사용을 위한 디자인 패턴 적용 - DI 적용을 위한 클라이언트/컨텍스트 분리

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -64,32 +64,10 @@ public class UserDao {
 	}
 
 	public void deleteAll() throws SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-		
-		try {
-			c = this.dataSource.getConnection();
-			
-			StatementStrategy strategy = new DeleteAllStatement();
-			ps = strategy.makePreparedStatement(c);
-			
-			ps.executeUpdate();
-		} catch (SQLException e) {
-			throw e;
-		} finally {
-			if (ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-				}
-			}
-			if (c != null) {
-				try {
-					c.close();
-				} catch (SQLException e) {
-				}
-			}
-		}
+		// 선정한 전략 클래스의 오브젝트 생성
+		StatementStrategy st = new DeleteAllStatement();
+		// 컨텍스트 호출. 전략 오브젝트 전달
+		jdbcContextWithStatementStrategy(st);
 	}
 	
 	public int getCount() throws SQLException {
@@ -126,6 +104,28 @@ public class UserDao {
 				} catch (SQLException e) {
 				}
 			}
+		}
+	}
+	
+	/*
+	 * StatementStrategy stmt : 클라이언트가 컨텍스트를 호출할 때 넘겨줄 전략 파라미터
+	 */
+	public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws 
+			SQLException {
+		Connection c = null;
+		PreparedStatement ps = null;
+		
+		try {
+			c = dataSource.getConnection();
+			
+			ps = stmt.makePreparedStatement(c);
+			
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (ps != null) { try { ps.close(); } catch (SQLException e) {} }
+			if (c != null) { try { c.close(); } catch (SQLException e) {} }
 		}
 	}
 
