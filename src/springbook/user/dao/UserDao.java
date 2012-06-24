726e819f6d0f9e5768faeea5233d3671e3d9c1aa
heejae won <hjwon@soribada.com>
2012-06-24 23:25:39 +0900

    3.3.2 전략과 클라이언트의 동거 - 익명 내부 클래스

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -24,26 +24,21 @@ public class UserDao {
 	}
 	
 	public void add(final User user) throws ClassNotFoundException, SQLException {
-		// 내부 클래스에서 외부의 변수를 사용할 때는 외부 변수는 반드시 final로 선언
-		// user 파라미터는 메소드 내부에서 변경될 일이 없으므로 final로 선언해도 무방
-		
-		// add() 메소드 내부에 선언된 로컬 클래스
-		class AddStatement implements StatementStrategy {
-			public PreparedStatement makePreparedStatement(Connection c)
-					throws SQLException {
-				PreparedStatement ps = 
-						c.prepareStatement("insert into users(id, name, password)" +
-								" values(?,?,?)");
-				ps.setString(1, user.getId());
-				ps.setString(2, user.getName());
-				ps.setString(3, user.getPassword());
-				
-				return ps;
-			}
-		}
-		
-		StatementStrategy st = new AddStatement();
-		jdbcContextWithStatementStrategy(st);
+		jdbcContextWithStatementStrategy(
+				new StatementStrategy() {
+					public PreparedStatement makePreparedStatement(Connection c)
+							throws SQLException {
+						PreparedStatement ps = 
+								c.prepareStatement("insert into users(id, name, password)" +
+										" values(?,?,?)");
+						ps.setString(1, user.getId());
+						ps.setString(2, user.getName());
+						ps.setString(3, user.getPassword());
+						
+						return ps;
+					}
+				}
+		);
 	}
 
 	public User get(String id) throws ClassNotFoundException, SQLException {
@@ -72,10 +67,15 @@ public class UserDao {
 	}
 
 	public void deleteAll() throws SQLException {
-		// 선정한 전략 클래스의 오브젝트 생성
-		StatementStrategy st = new DeleteAllStatement();
-		// 컨텍스트 호출. 전략 오브젝트 전달
-		jdbcContextWithStatementStrategy(st);
+		jdbcContextWithStatementStrategy(
+				new StatementStrategy() {
+					public PreparedStatement makePreparedStatement(Connection c) throws
+							SQLException {
+						PreparedStatement ps = c.prepareStatement("delete from users");
+						return ps;
+					}
+				}
+		);
 	}
 	
 	public int getCount() throws SQLException {
