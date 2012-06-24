6219a0ee6d26aba86fb2844fdeaa0a52c3301136
heejae won <hjwon@soribada.com>
2012-06-25 01:26:27 +0900

    3.5.3 템플릿/콜백의 응용 - 중복의 제거와 템플릿/콜백 설계

--- src/springbook/learningtest/template/CalcSumTest.java
+++ src/springbook/learningtest/template/CalcSumTest.java
@@ -5,14 +5,24 @@ import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalcSumTest {
-	@Test
-	public void sumOfNumbers() throws IOException {
-		Calculator calculator = new Calculator();
-		int sum = calculator.calcSum(getClass().getResource(
-				"numbers.txt").getPath());
-		assertThat(sum, is(10));
+	Calculator calculator;
+	String numFilepath;
+	
+	@Before public void setUp() {
+		this.calculator = new Calculator();
+		this.numFilepath = getClass().getResource("numbers.txt").getPath();
 	}
+	
+	@Test public void sumOfNumbers() throws IOException {
+		assertThat(calculator.calcSum(this.numFilepath), is(10));
+	}
+	
+	@Test public void multiplyOfNumbers() throws IOException {
+		assertThat(calculator.calcMultiply(this.numFilepath), is(24));
+	}
+	
 }
--- src/springbook/learningtest/template/Calculator.java
+++ src/springbook/learningtest/template/Calculator.java
@@ -6,17 +6,44 @@ import java.io.IOException;
 
 public class Calculator {
 	public Integer calcSum(String filepath) throws IOException {
+		BufferedReaderCallback sumCallback = 
+				new BufferedReaderCallback() {
+					public Integer doSomethingWithReader(BufferedReader br) throws
+							IOException {
+						Integer sum = 0;
+						String line = null;
+						while((line = br.readLine()) != null) {
+							sum += Integer.valueOf(line);
+						}
+						return sum;
+					}
+		};
+		return fileReadTemplate(filepath, sumCallback);
+	}
+	
+	public Integer calcMultiply(String filepath) throws IOException {
+		BufferedReaderCallback sumCallback = 
+				new BufferedReaderCallback() {
+					public Integer doSomethingWithReader(BufferedReader br) throws
+							IOException {
+						Integer multiply = 1;
+						String line = null;
+						while((line = br.readLine()) != null) {
+							multiply *= Integer.valueOf(line);
+						}
+						return multiply;
+					}
+		};
+		return fileReadTemplate(filepath, sumCallback);
+	}
+	
+	public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback)
+			throws IOException {
 		BufferedReader br = null;
-		
 		try {
 			br = new BufferedReader(new FileReader(filepath));
-			Integer sum = 0;
-			String line = null;
-			while((line = br.readLine()) != null) {
-				sum += Integer.valueOf(line);
-			}
-			
-			return sum;
+			int ret = callback.doSomethingWithReader(br);
+			return ret;
 		}
 		catch(IOException e) {
 			System.out.println(e.getMessage());
@@ -29,4 +56,5 @@ public class Calculator {
 			}
 		}
 	}
+	
 }
